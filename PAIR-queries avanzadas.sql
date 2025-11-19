8ea61d87b9c7aa16e6032bd354dc1470118fd789
Patricia Anaya Morales <pat.anaya.morales@gmail.com>
2025-11-19 17:17:12 +0100

    ejercicios corregidos

--- PAIR-queries avanzadas.sql
+++ PAIR-queries avanzadas.sql
@@ -19,10 +19,21 @@ FROM products
 WHERE UnitPrice > (SELECT AVG(UnitPrice) FROM products)
 ORDER BY UnitPrice DESC;
 
+--ejercicio resuelto
+SELECT AVG(UnitPrice) as Precio_Medio  
+FROM products;
+
+SELECT DISTINCT ProductName, UnitPrice  
+FROM products 
+WHERE UnitPrice > (28.86)  
+ORDER BY UnitPrice DESC;
+
 /*Qué productos se han descontinuado*/
 SELECT COUNT(*)
 FROM products
 WHERE discontinued != 0;
+
+
 
 /*Detalles de los productos de la query anterior*/
 SELECT ProductID, ProductName
@@ -43,17 +54,29 @@ WHERE ShippedDate IS NOT NULL
 GROUP BY EmployeeID
 ORDER BY EmployeeID;
 
+
+-- BONUS
 /*Números de pedidos por día*/
 SELECT year(OrderDate) as AÑO, month(OrderDate) as MES, day(OrderDate) as DIA, count(orderID)
 FROM orders
 GROUP BY AÑO, MES, DIA
 ORDER BY AÑO, MES, DIA;
+-- corregido
+SELECT COUNT(*) AS OrderCount, DAY(OrderDate) AS OrderDay, MONTH(OrderDate) AS OrderMonth, YEAR(OrderDate) AS OrderYear  
+FROM orders 
+GROUP BY OrderDate  
+ORDER BY OrderDate;  
 
 /*Número de pedidos por mes y año*/
 SELECT year(OrderDate) as AÑO, month(OrderDate) as MES, count(orderID) AS PEDIDOS
 FROM orders
 GROUP BY AÑO, MES
 ORDER BY AÑO, MES;
+-- corregido
+SELECT COUNT(*) AS OrderCount, MONTH(OrderDate) AS OrderMonth, YEAR(OrderDate) AS OrderYear  
+FROM orders  
+GROUP BY  OrderDate  
+ORDER BY OrderDate;
 
 /*lecciona las ciudades con 4 o más empleadas*/
 SELECT City, COUNT(*) AS empleadas
@@ -62,7 +85,6 @@ GROUP BY City
 HAVING empleadas >= 4;
 
 /*Crea una nueva columna basándonos en la cantidad monetaria*/
-
 SELECT OrderID, SUM(UnitPrice * Quantity) AS CostoTotalPedido,
 CASE
 	WHEN SUM(UnitPrice * Quantity) >= 2000 THEN 'Alto'
